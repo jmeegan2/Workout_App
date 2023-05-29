d52e9e33a4a2a97c84e25f8dcfd122ef79e50824
James Meegan <58824024+jmeegan2@users.noreply.github.com>
2023-05-29 13:02:59 -0400

    Initial Commit

---
+++ Workout_App/Workout_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_AppApp.swift
+//  Workout_App
+//
+//  Created by James Meegan on 5/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Workout_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
