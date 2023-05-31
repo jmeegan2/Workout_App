8f710b4b64bc46b6df994431d246b9b39688d543
James Meegan <58824024+jmeegan2@users.noreply.github.com>
2023-05-30 22:07:24 -0400

    nav bar

---
+++ README.md
@@ -0,0 +1,32 @@
+User Requirements so far
+
+* User Registration and Profile Management,
+* Exercise Tracking for workouts seamless,
+* Goals to set, progress to witness.
+* Charts and graphs, progress complete,
+* Personalized plans for fitness,
+* Connect with friends, a social witness.
+* Notifications and reminders to stay on track,
+* Security and privacy, no data to lack,
+* Create an account, manage with ease,
+* A fitness app to help you seize.
+* See a chart of days worked out,
+* Track workout time, no room for doubt,
+* Set weekly or monthly goals to strive,
+* Measure progress, keep the fire alive.
+* Recommendations based on govermental body guidelines on health and fitness
+
+Problem solving: the need to pay for a fitness tracker, this will be free and fairly simple for the user to use, Also it will be tailored to my specific workout needs and will be customizable for others
+
+
+App Design Guide: 
+
+* Nav Bar
+* Login and registration page
+* Home page
+* Workout page
+* Create new workout page
+* Settings page
+* Some way to view guidelines on fitness provided from goverment or heart association 
+
+
--- Workout_App/Views/ContentView.swift
+++ Workout_App/Views/ContentView.swift
@@ -7,15 +7,72 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+import SwiftUI
+import BottomBar_SwiftUI
+
+let items: [BottomBarItem] = [
+    BottomBarItem(icon: "house.fill", title: "Home", color: .purple),
+    BottomBarItem(icon: "dumbbell", title: "Workout", color: .pink),
+    BottomBarItem(icon: "chart.bar", title: "Charts", color: .orange),
+    BottomBarItem(icon: "gear.circle", title: "Settings", color: .blue)
+]
+
+struct BasicView: View {
+    let item: BottomBarItem
+
+    var detailText: String {
+    "\(item.title) Detail"
+}
+
+
+var destination: some View {
+    Text(detailText)
+        .navigationBarTitle(Text(detailText))
+}
+
+var navigateButton: some View {
+    NavigationLink(destination: destination) {
+        ZStack {
+            Rectangle()
+                .fill(item.color)
+                .cornerRadius(8)
+                .frame(height: 52)
+                .padding(.horizontal)
+
+            Text("Navigate")
+                .font(.headline)
+                .foregroundColor(.white)
         }
-        .padding()
+    }
+}
+
+
+var body: some View {
+    VStack {
+        Spacer()
+
+        Spacer()
+
+        navigateButton
+        }
+    }
+}
+
+struct ContentView : View {
+    @State private var selectedIndex: Int = 0
+
+    var selectedItem: BottomBarItem {
+        items[selectedIndex]
+    }
+
+var body: some View {
+        NavigationView {
+            VStack {
+                BasicView(item: selectedItem)
+                    .navigationBarTitle(Text(selectedItem.title))
+                BottomBar(selectedIndex: $selectedIndex, items: items)
+            }
+        }
     }
 }
 
